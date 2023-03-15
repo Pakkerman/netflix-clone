721b71a041431a101c766af44add00599a98eb0c
Pakkerman <Pakkermandev.gmail.com>
2023-03-15 16:36:37 +0800

    implemented billboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Billboard from "@/components/Billboard";
 import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
@@ -14,6 +15,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   );
 }
